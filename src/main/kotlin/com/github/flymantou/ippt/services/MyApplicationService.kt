ce9b6f6dc6beaf1822e3e181cd3eb0e56a1e49ad
GitHub Action <action@github.com>
2021-06-11 01:49:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/flymantou/ippt/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/flymantou/ippt/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.flymantou.ippt.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.flymantou.ippt.MyBundle
 
 class MyApplicationService {
 
